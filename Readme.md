e201cfcde01ee1abad2d8309fecc733883e3d47c
Tommy Parnell <tparnell8@gmail.com>
2016-01-16 13:34:22 -0500

    init

---
+++ Readme.md
@@ -0,0 +1,9 @@
+This is a simple program that constructs a decklist in your clipboard that jinteki.net tolerates (windows only sorry!)
+
+
+Build the program.
+Then call jinteki.exe IdOfNetrunnerDbDeck
+
+You can find the id in the url of a decklist
+
+for example http://netrunnerdb.com/en/decklist/30658/sea-scorched-earth-advisers-inc is a valid url the numbers after decklist but before sea-scorched is a valid id so in this case its 30658
---
+++ src/NetrunnerdbToJinteki/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetrunnerDb.Net;
+
+namespace NetrunnerdbToJinteki
+{
+    public class Program
+    {
+        [STAThread()]
+        public static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Input id of decklist");
+                Environment.Exit(1);
+            }
+            var stringBuilder = new StringBuilder();
+            new Repository()
+                .GetDecklist(args[0])
+                .First()
+                .Cards
+                .Select(a => new { Card = new Repository().GetCard(a.Key).First(), Count = a.Value })
+                .Where(a => !string.Equals(a.Card.Type, "Identity", StringComparison.OrdinalIgnoreCase))
+                .ForEach(a => stringBuilder.AppendLine($"{a.Count} {a.Card.Title}"));
+
+            Clipboard.SetText(stringBuilder.ToString());
+        }
+    }
+}
