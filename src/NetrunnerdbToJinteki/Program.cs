f0db4e829930be8599ae7da6c98f807ad48aa764
Tommy Parnell <tparnell8@gmail.com>
2016-01-17 10:21:06 -0500

    more fluent syntax

--- src/NetrunnerdbToJinteki/Program.cs
+++ src/NetrunnerdbToJinteki/Program.cs
@@ -18,16 +18,25 @@ namespace NetrunnerdbToJinteki
                 Console.WriteLine("Input id of decklist");
                 Environment.Exit(1);
             }
-            var stringBuilder = new StringBuilder();
-            new Repository()
-                .GetDecklist(args[0])
-                .First()
-                .Cards
-                .Select(a => new { Card = new Repository().GetCard(a.Key).First(), Count = a.Value })
-                .Where(a => !string.Equals(a.Card.Type, "Identity", StringComparison.OrdinalIgnoreCase))
-                .ForEach(a => stringBuilder.AppendLine($"{a.Count} {a.Card.Title}"));
 
-            Clipboard.SetText(stringBuilder.ToString());
+            var data = new Repository()
+                 .GetDecklist(args[0])
+                 .First()
+                 .Cards
+                 .Select(a => new { Card = new Repository().GetCard(a.Key).First(), Count = a.Value })
+                 .Where(a => !string.Equals(a.Card.Type, "Identity", StringComparison.OrdinalIgnoreCase))
+                 .Select(a => $"{a.Count} {a.Card.Title}")
+                 .Join(Environment.NewLine);
+
+            Clipboard.SetText(data);
+        }
+    }
+
+    public static class extension
+    {
+        public static string Join(this IEnumerable<string> list, string seperator = "")
+        {
+            return string.Join(seperator, list);
         }
     }
 }
